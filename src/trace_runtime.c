34683afd6f6b55effabca157c9b1960e548a9728
Oised <pedrodesiodavoli@gmail.com>
2026-05-02 23:21:43 -0300

    feat: implement launch_tracee and wait_for_initial_stop

--- src/trace_runtime.c
+++ src/trace_runtime.c
@@ -37,7 +37,7 @@ static void fill_event_from_regs(pid_t pid,
 static pid_t launch_tracee(char *const argv[])
 {
     /*
-     * TODO Semana 2:
+     * DONE Semana 2:
      *
      * Crie o processo monitorado.
      *
@@ -51,23 +51,55 @@ static pid_t launch_tracee(char *const argv[])
      *   - retornar o pid do filho
      *
      * Em erro, imprima uma mensagem com perror() e retorne -1.
-     */
-    fprintf(stderr, "erro: TODO Semana 2: implementar launch_tracee()\n");
-    return -1;
+    */
+
+    // fork
+    pid_t cpid = fork();
+    if (cpid < 0) {
+        perror("Erro no fork!\n");
+        return -1;
+    }
+
+    // filho
+    if (cpid == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        raise(SIGSTOP);
+        execvp(argv[0], argv);
+    }
+
+    // pai
+    else {
+        return cpid;
+    }
+
 }
 
 static int wait_for_initial_stop(pid_t child)
 {
     /*
-     * TODO Semana 2:
+     * DONE Semana 2:
      *
      * O filho chama raise(SIGSTOP) antes de executar o programa alvo.
      * O pai precisa esperar essa parada inicial com waitpid().
      *
      * Retorne 0 se o filho parou como esperado, -1 em erro.
-     */
-    fprintf(stderr, "erro: TODO Semana 2: implementar wait_for_initial_stop()\n");
-    return -1;
+    */
+
+    int status;
+    if (waitpid(child, &status, 0) < 0) {
+        perror("Erro no waitpid!\n");
+        return -1;
+    }
+    else{
+        if (!WIFSTOPPED(status)) {
+            perror("Erro, filho não parou!\n"); 
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+
 }
 
 static int configure_trace_options(pid_t child)
