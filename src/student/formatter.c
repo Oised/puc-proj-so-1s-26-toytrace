e04f840cb3432a5e3929d628202aad601bb7a39d
RicardoAzam <razambuja99@gmail.com>
2026-06-07 18:59:52 -0300

    fix on week 5

--- src/student/formatter.c
+++ src/student/formatter.c
@@ -6,6 +6,8 @@
 
 #include <string.h>
 
+#include <sys/syscall.h> 
+
 void student_debug_raw_event(const struct syscall_event *ev,
                              char *buf,
                              size_t bufsz)
@@ -90,21 +92,21 @@ void student_format_event(const struct syscall_event *ev,
     */
 
     // if read ->
-    if (strcmp(syscall_name(ev->syscall_no), "read") == 0) {
+     if (ev->syscall_no == SYS_read) {
         snprintf(buf, bufsz, "read(%#lx, %#lx, %#lx) = %ld",
                  ev->args[0], ev->args[1], ev->args[2], ev->ret);
         return;
     }
 
     // if write ->
-    if (strcmp(syscall_name(ev->syscall_no), "write") == 0) {
+    if (ev->syscall_no == SYS_write) {
         snprintf(buf, bufsz, "write(%#lx, %#lx, %#lx) = %ld",
                  ev->args[0], ev->args[1], ev->args[2], ev->ret);
         return;
     }
 
     // if openat ->
-    if (strcmp(syscall_name(ev->syscall_no), "openat") == 0) {
+    if (ev->syscall_no == SYS_openat) {
         char path[256];
         if (read_child_string(ev->pid, ev->args[1], path, sizeof(path)) < 0)
             strcpy(path, "<ilegivel>");
@@ -114,7 +116,7 @@ void student_format_event(const struct syscall_event *ev,
     }
 
     // if execve ->
-    if (strcmp(syscall_name(ev->syscall_no), "execve") == 0) {
+    if (ev->syscall_no == SYS_execve) {
         char path[256];
         if (read_child_string(ev->pid, ev->args[0], path, sizeof(path)) < 0)
             strcpy(path, "<ilegivel>");
@@ -124,19 +126,15 @@ void student_format_event(const struct syscall_event *ev,
     }
 
     // if exit_group ->
-    if (strcmp(syscall_name(ev->syscall_no), "exit_group") == 0) {
+    if (ev->syscall_no == SYS_exit_group) {
         snprintf(buf, bufsz, "exit_group(%#lx) = %ld", ev->args[0], ev->ret);
         return;
     }
 
-// if não é read, write, openat, execve ou exit_group ->
+    // default case ->
     snprintf(buf, bufsz, "%s(%#lx, %#lx, %#lx, %#lx, %#lx, %#lx) = %ld",
              syscall_name(ev->syscall_no),
-             ev->args[0],
-             ev->args[1],
-             ev->args[2],
-             ev->args[3],
-             ev->args[4],
-             ev->args[5],
+             ev->args[0], ev->args[1], ev->args[2],
+             ev->args[3], ev->args[4], ev->args[5],
              ev->ret);
 }
